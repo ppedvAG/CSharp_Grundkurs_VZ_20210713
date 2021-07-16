380f7fee6fe201f8323a416ded3c4b8516585558
Benedikt Siebert <BenediktS@ppedv.de>
2021-07-16 16:51:35 +0200

    16

---
+++ Uebungen/Modul016_Console/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace Lab16_Console
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static string apiUrl = "api/todoitems/";
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("https://meineurl/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            TodoItem item = new TodoItem();
+            item.Name = "Mein neues Item";
+            item.IsComplete = false;
+
+            try
+            {
+                Uri url = Create(item);
+                Console.WriteLine($"Erstellt mit URL {url}");
+
+                item = Get(url.PathAndQuery);
+                Console.WriteLine(item.Info());
+
+                item.Name = "Ich habe einen neuen Namen!";
+                item = Update(item);
+
+                Console.WriteLine(item.Info());
+
+                Console.ReadLine();
+
+                HttpStatusCode code = Delete(item.Id);
+                Console.WriteLine($"Wurde gelöscht mit Code: {code}");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        static string CreateJson(TodoItem item)
+        {
+            return JsonSerializer.Serialize(item, typeof(TodoItem));
+        }
+
+        static TodoItem CreateTodoItem(string json)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                return JsonSerializer.Deserialize<TodoItem>(json, options);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        static List<TodoItem> CreateTodoItems(string json)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                return JsonSerializer.Deserialize<List<TodoItem>>(json, options);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        static TodoItem Get(long id)
+        {
+            return Get($"{apiUrl}{id}");
+        }
+
+        static TodoItem Get(string path)
+        {
+            TodoItem item = null;
+
+            HttpResponseMessage response = client.GetAsync(path).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                item = CreateTodoItem(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return item;
+        }
+
+        static Uri Create(TodoItem item)
+        {
+            HttpContent content = new StringContent(CreateJson(item), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(apiUrl, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Headers.Location; 
+            }
+            return null;
+        }
+
+        static TodoItem Update(TodoItem item)
+        {
+
+            TodoItem newItem = null; 
+            HttpContent content = new StringContent(CreateJson(item), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync($"{apiUrl}{item.Id}", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                newItem = CreateTodoItem(response.Content.ReadAsStringAsync().Result); 
+            }
+
+            return newItem; 
+        }
+
+        static HttpStatusCode Delete(long id)
+        {
+            HttpResponseMessage response = client.DeleteAsync($"{apiUrl}{id}").Result;
+            return response.StatusCode; 
+        }
+    }
+
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public string Info()
+        {
+            return $"Id: {Id}\nName:{Name}\nIdComplete:{IsComplete}";
+        }
+    }
+}
---
+++ Uebungen/Modul016_WPF/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace Lab16_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+        string apiUrl = "api/todoitems/";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client.BaseAddress = new Uri("https://lab16-api-bsi.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async void btnSuche_Click(object sender, RoutedEventArgs e)
+        {
+            string suche = tbSuche.Text;
+            HttpResponseMessage response = await client.GetAsync($"{apiUrl}{suche}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                List<TodoItem> items = null;
+                if (suche.Contains("-"))
+                {
+                    items = CreateTodoItems(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    TodoItem item = CreateTodoItem(await response.Content.ReadAsStringAsync());
+                    await UpdateUI(item);
+                    items = new List<TodoItem>() { item };
+                }
+                dgItems.ItemsSource = items; 
+
+            }
+            else
+            {
+                MessageBox.Show("Nichts gefunden..");
+            }
+        }
+
+        private async void btnBearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+            TodoItem item = new TodoItem();
+            item.Id = Convert.ToInt64(tbId.Text);
+            item.Name = tbName.Text;
+            item.IsComplete = cbErledigt.IsChecked ?? true;
+
+            HttpContent content = new StringContent(CreateJson(item), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync($"{apiUrl}{item.Id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TodoItem newItem = CreateTodoItem(await response.Content.ReadAsStringAsync());
+                await UpdateUI(newItem);
+                dgItems.ItemsSource = new List<TodoItem>() { newItem };
+            }
+            else
+            {
+                MessageBox.Show($"Es ist ein Fehler aufgetreten! Status:{response.StatusCode}");
+            }
+        }
+
+        private async void btnNeu_Click(object sender, RoutedEventArgs e)
+        {
+            TodoItem item = new TodoItem();
+            item.Name = tbName.Text;
+            item.IsComplete = cbErledigt.IsChecked ?? false;
+
+            HttpContent content = new StringContent(CreateJson(item), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TodoItem newItem = CreateTodoItem(await response.Content.ReadAsStringAsync());
+                await UpdateUI(newItem);
+                dgItems.ItemsSource = new List<TodoItem>() { newItem };
+            }
+            else
+            {
+                MessageBox.Show($"Es ist ein Fehler aufgetreten! Status:{response.StatusCode}");
+            }
+        }
+
+        private async void btnEntfernen_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Soll das Item gelöscht werden?", "Löschen", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                long id = Convert.ToInt64(tbId.Text);
+                HttpResponseMessage response = await client.DeleteAsync($"{apiUrl}{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Item wurde erfolgreich gelöscht!");
+                }
+                else
+                {
+                    MessageBox.Show($"Es ist ein Fehler aufgetreten! Status:{response.StatusCode}");
+                }
+            }
+        }
+        private async Task UpdateUI(TodoItem item)
+        {
+            tbId.Text = item?.Id.ToString();
+            tbName.Text = item?.Name;
+            cbErledigt.IsChecked = item?.IsComplete;
+        }
+
+        static string CreateJson(TodoItem item)
+        {
+            return JsonConvert.SerializeObject(item);
+        }
+
+        static TodoItem CreateTodoItem(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<TodoItem>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        static List<TodoItem> CreateTodoItems(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<List<TodoItem>>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
